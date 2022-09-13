r7kamura/diary

Add link element for RSS auto discovery

settings.js
@@ -1,3 +1,6 @@
+const basePath = "/diary";
+
 module.exports = {
-  siteBaseUrl: process.env.BASE_URL || "https://r7kamura.github.io/diary",
+  basePath,
+  siteBaseUrl: process.env.BASE_URL || `https://r7kamura.github.io${basePath}`,
 };

tf0053/blog01

settings.js
@@ -1,3 +1,6 @@
+const basePath = "/blog01";
+
 module.exports = {
-  siteBaseUrl: process.env.BASE_URL || "https://r7kamura.github.io/diary",
+  basePath,
+  siteBaseUrl: process.env.BASE_URL || `https://tf0053.github.io${basePath}`,
 };